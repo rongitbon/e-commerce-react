32d5592a907e5d16b43cdbf8b625e0c59feb7c46
rongitbon <rongitbon@gmail.com>
2019-08-08 16:13:54 +0300

    Adding header components

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.jsx';
 import ShopPage from './pages/shop/shop.jsx';
+import Header from './components/header/header.jsx';
 
 import './App.css';
 
@@ -15,9 +16,10 @@ const HatsPage = () => (
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/:hi' component={ShopPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
